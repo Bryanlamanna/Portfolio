860f39f5064fea4cf36919de2e874e0ca9372824
Bryan Lamanna <lamannabryan@gmail.com>
2023-10-13 11:46:20 -0300

    initial uploads

---
+++ script.js
@@ -0,0 +1,39 @@
+var theme = 1;
+
+function fade() {
+  
+    var body = document.body;
+    var botoeshd = document.querySelectorAll('.botaoheader');
+    var botoesbd = document.querySelectorAll('.botaobody');
+
+    if (theme == 1){
+    body.classList.add('darkmode');
+    
+    for (var i = 0 ; i < botoesbd.length ; i++) {
+        botoesbd[i].style.color = 'white';
+    }
+
+    for (var i = 0 ; i < botoeshd.length ; i++) {
+        botoeshd[i].style.color = 'white';
+    }
+    theme=2;
+    return;
+    }
+
+    body.classList.remove('darkmode');
+        
+    for (var i = 0 ; i < botoesbd.length ; i++) {
+        botoesbd[i].style.color = 'black';
+    }
+
+    for (var i = 0 ; i < botoeshd.length ; i++) {
+        botoeshd[i].style.color = 'black';
+    }
+    theme=1;      
+}
+
+function fadetolight() {
+    var body = document.body;
+
+    body.classList.remove('darkmode');
+}
